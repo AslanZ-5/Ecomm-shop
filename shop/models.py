ff67fda5d5a8443529d2ecb2589e53ad3af47012
Aslan <asl.zurabov@gmail.com>
2021-10-17 21:04:22 +0300

    latest products ordered by model name

--- shop/models.py
+++ shop/models.py
@@ -5,13 +5,20 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class LatestProductsManager:
+
     @staticmethod
-    def get_products_for_models(self, *args, **kwargs):
+    def get_products_for_models(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
         products = []
         ct_models = ContentType.objects.filter(model__in=args)
         for ct_model in ct_models:
             model_product = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
             products.extend(model_product)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model = with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to),reverse=True)
         return products
 
 
