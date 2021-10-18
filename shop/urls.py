3610445814199e58f61dca76eea0b6aa98c1395c
Aslan <asl.zurabov@gmail.com>
2021-10-18 21:00:00 +0300

    added detail view and method which get url from multiple models

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,6 @@ from shop import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('product/<str:ct_model>/<str:slug>/',views.ProductDetailView.as_view(), name='product_detail')
 ]
 
--- shop/views.py
+++ shop/views.py
@@ -7,4 +7,17 @@ def index(request):
     return render(request, 'shop/base.html')
 
 
+class ProductDetailView(DetailView):
+    CT_MODEL = {
+        'laptop': Laptop,
+        'smartphone': SmartPhone
+    }
 
+    def dispatch(self, request, *args, **kwargs):
+        self.model = self.CT_MODEL[kwargs['ct_model']]
+        self.queryset = self.model._base_manager.all()
+        return super().dispatch(request, *args, **kwargs)
+
+    context_object_name = 'product'
+    template_name = 'shop/product_detail.html'
+    slug_url_kwarg = 'slug'
