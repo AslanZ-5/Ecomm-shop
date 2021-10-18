a15ad443dc523d3900a59f3362f637a0a31d6b14
Aslan <asl.zurabov@gmail.com>
2021-10-18 15:06:30 +0300

    adding notice about resolution in image form in admin page

--- shop/admin.py
+++ shop/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import *
 from django import forms
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm
+
+
+class ImageResolutionNotice(ModelForm):
+    resolution = (400, 400)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = f"Upload an image with the minimum resolution {self.resolution[0]}x{self.resolution[1]} "
 
 
 class LaptopAdmin(admin.ModelAdmin):
+    form = ImageResolutionNotice
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
             return ModelChoiceField(Category.objects.filter(slug='laptop'))
@@ -12,6 +22,7 @@ class LaptopAdmin(admin.ModelAdmin):
 
 
 class SmartPhoneAdmin(admin.ModelAdmin):
+    form = ImageResolutionNotice
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
             return ModelChoiceField(Category.objects.filter(slug='smartphone'))
@@ -21,6 +32,6 @@ class SmartPhoneAdmin(admin.ModelAdmin):
 admin.site.register(Cart)
 admin.site.register(Customer)
 admin.site.register(Category)
-admin.site.register(Laptop,LaptopAdmin)
-admin.site.register(SmartPhone,SmartPhoneAdmin)
+admin.site.register(Laptop, LaptopAdmin)
+admin.site.register(SmartPhone, SmartPhoneAdmin)
 admin.site.register(CartProduct)
