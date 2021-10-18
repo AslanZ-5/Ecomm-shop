ba757a34490270d340100694e91c7559bd4edc14
Aslan <asl.zurabov@gmail.com>
2021-10-18 16:12:01 +0300

    raise error if image size is more or less than valid size

--- shop/admin.py
+++ shop/admin.py
@@ -7,19 +7,26 @@ from PIL import Image
 
 
 class ImageResolutionNotice(ModelForm):
-    resolution = (400, 400)
+    min_resolution = (199, 199)
+    max_resolution = (500,500)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = f"Upload an image with the minimum resolution {self.resolution[0]}x{self.resolution[1]} "
+        self.fields[
+            'image'].help_text = f"Upload an image with the minimum resolution {self.min_resolution[0]}x{self.min_resolution[1]} "
 
     def clean_image(self):
         image = self.cleaned_data['image']
         img = Image.open(image)
-        min_width,min_height = self.resolution
+        min_width, min_height = self.min_resolution
+        max_width, max_height = self.max_resolution
         if img.width < min_width or img.height < min_height:
             raise ValidationError("The image resolution is less than the minimum")
+        if img.width > max_width or img.height > max_height:
+            raise ValidationError("The image resolution is more than the minimum")
         return image
+
+
 class LaptopAdmin(admin.ModelAdmin):
     form = ImageResolutionNotice
 
@@ -31,6 +38,7 @@ class LaptopAdmin(admin.ModelAdmin):
 
 class SmartPhoneAdmin(admin.ModelAdmin):
     form = ImageResolutionNotice
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
             return ModelChoiceField(Category.objects.filter(slug='smartphone'))
