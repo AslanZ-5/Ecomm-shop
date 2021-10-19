760639fe303b765ebcc39fed9dcd93d25788b918
Aslan <asl.zurabov@gmail.com>
2021-10-19 10:16:23 +0300

    template inheritance

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from shop import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('product/<str:ct_model>/<str:slug>/',views.ProductDetailView.as_view(), name='product_detail')
 ]
 
