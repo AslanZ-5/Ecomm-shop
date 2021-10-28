ccd727cbf81d00591784bc6218bdf8a92162fe02
Aslan <asl.zurabov@gmail.com>
2021-10-28 14:42:45 +0300

    filtering smartphone api

--- shop/api/api_view.py
+++ shop/api/api_view.py
@@ -12,3 +12,11 @@ class SmartphoneListAPIView(ListAPIView):
     serializer_class = SmartphoneSerializer
     queryset = SmartPhone.objects.all()
 
+    def get_queryset(self):
+        slug = self.request.query_params.get('slug')
+        price = self.request.query_params.get('price')
+        ww = {'price':price,'slug':slug}
+        if slug:
+            return super().get_queryset().filter(**ww)
+        else:
+            return super().get_queryset()
