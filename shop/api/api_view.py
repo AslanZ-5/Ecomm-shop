d88f7da387de4d7f3f1dd74956533e0955858ba4
Aslan <asl.zurabov@gmail.com>
2021-10-28 16:30:39 +0300

    list create view for categories

--- shop/api/api_view.py
+++ shop/api/api_view.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView
 from .serializers import CategorySerializer, SmartphoneSerializer, LaptopSerializer, CustomerSerializer
 from shop.models import Category, SmartPhone, Customer, Laptop
 from rest_framework.filters import SearchFilter
@@ -21,7 +21,7 @@ class CategoryPagination(PageNumberPagination):
         ]))
 
 
-class CategoryListApiView(ListAPIView):
+class CategoryListApiView(ListCreateAPIView):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
     pagination_class = CategoryPagination
